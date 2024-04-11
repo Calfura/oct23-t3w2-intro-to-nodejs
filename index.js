a1b70b413a43ca373e8b61159bd0f94e8566faf8
Calfura <travis_wilson999@hotmail.com>
2024-04-11 19:38:28 +1000

    feat: added user prompt

--- index.js
+++ index.js
@@ -2,16 +2,76 @@ require('dotenv').config();
 // The above is equivalent to the next two lines:
 // const dotenv = require('dotenv');
 // dotenv.config();
+const pokemonPrinterFile = require("./pokemonPrinter");
 
-console.log(process.env.ENVIRONMENT_MESSAGE);
+
+// import blahblah from 'dotenv';
+
+
+
+// Style 1 for setting up prompt-sync:
+var prompt = require('prompt-sync')({sigint: true});
+
+// Style 2 for setting up prompt-sync:
+let promptPackage = require('prompt-sync');
+let promptInstance = promptPackage({sigint: true});
+
+
+//
+// get input from the user.
+//
+// var n = prompt('What number of Pokemon do you want to see?');
+
+
+let userWantsToExit = false;
+
+do {
+
+	let n = parseFloat(prompt("What number of Pokemon do you want to see?"));
+	console.log("You entered " + n);
+
+	let userInputToExit = prompt("Would you like to try again?");
+
+	if (userInputToExit == "y"){
+		userWantsToExit = false;
+	} else {
+		userWantsToExit = true;
+	}
+
+} while (userWantsToExit == false);
+
+
+
+
+// console.log("User entered: " + n);
+// console.log(typeof(n));
+
+// let nAsNumber = parseFloat(n);
+// console.log(typeof(nAsNumber));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(process.env.ENVIRONMENT_MESSAGE)
+
 
 
 console.log("Terminal app is running!");
 
-const pokemon = require('pokemon');
 
-let randomPokemon = pokemon.random();
-console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+pokemonPrinterFile.pokemonPrinter();
 
-console.log("Bye bye, terminal app finished!")
-
+console.log("Bye bye, terminal app finished!");
--- pokemonPrinter.js
+++ pokemonPrinter.js
@@ -1,9 +1,13 @@
 
 
-function pokeonPrinter(){
-    const pokemon = require('pokemon');
+function pokemonPrinter(){
+	const pokemon = require('pokemon');
 
-    let randomPokemon = pokemon.random();
+	let randomPokemon = pokemon.random();
 
-    console.log(`Today's lucky Pokemon is ${randomPokemon}`)
+	console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+}
+
+module.exports = {
+	pokemonPrinter
 }
